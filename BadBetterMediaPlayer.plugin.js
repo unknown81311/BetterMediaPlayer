f5d00aea800b31c0cff8ac919f18960466770536
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2025-01-30 21:18:44 -0700

    Create BadBetterMediaPlayer.plugin.js

---
+++ BadBetterMediaPlayer.plugin.js
@@ -0,0 +1,14 @@
+/**
+ * @name BadBetterMediaPlayer
+ * @version 1.2.12
+ * @author unknown81311_&_Doggybootsy
+ * @description Deny this plugin please
+ * @authorLink https://betterdiscord.app/plugin?id=377
+ * @source https://github.com/unknown81311/BetterMediaPlayer
+ * @updateUrl https://raw.githubusercontent.com/unknown81311/BetterMediaPlayer/main/BetterMediaPlayer.plugin.js
+ * @invite yYJA3qQE5F
+ */
+
+module.exports = () => {
+  return { start(), stop() {} };
+}
